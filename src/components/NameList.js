698fb145561a77b58cf06227212c259e1f0978d6
Sivakumar U <itsmesivakumar21@gmail.com>
2024-10-24 17:18:46 -0400

    Added name list to print the names in the list

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
 function NameList() {
-    const names=['vijay','ajay','sujay']
-  return (
-    <div>
-      <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2>
-    </div>
-  )
+    const persons=[
+        {
+            id:1,
+            name:'vijay',
+            age:22,
+            skill:'Java'
+        },
+        {
+            id:2,
+            name:'ajay',
+            age:23,
+            skill:'python'
+        },
+        {
+            id:3,
+            name:'sujay',
+            age:24,
+            skill:'react'
+        }
+    ]
+    
+    const personList = persons.map(person=>(
+        <h1>
+            I am {person.name}.I am {person.age} years old. I know {person.skill}.
+        </h1>
+    ))
+    return <div>{personList}</div>
+ 
 }
 
 export default NameList
