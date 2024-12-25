5bb664732db2960302f5154e8b763dd467c7b5f6
Sivakumar U <itsmesivakumar21@gmail.com>
2024-12-24 19:03:53 -0500

    Using Forward Ref method in the functional class which is child class we use to focus into the input text by clicking button from the parent class.

--- src/App.js
+++ src/App.js
@@ -22,15 +22,18 @@ import MyPureComp from './components/MyPureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      <FRParentInput/>
       
       
       {
       /*<Counter/>
+
+      <FocusInput/>
 
       <RefsDemo/>
 
