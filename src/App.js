c1ad6b49323a248d97e6a81af39e74ff17b4978c
Sivakumar U <itsmesivakumar21@gmail.com>
2024-12-24 19:30:57 -0500

    Created a increment count method to know the no of times we clicked the button.

--- src/App.js
+++ src/App.js
@@ -24,14 +24,17 @@ import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
 import PortalDemo from './components/PortalDemo';
+import ClickCounter from './components/ClickCounter';
 
 function App() {
   return (
     <div className="App">
-      <PortalDemo/>
+      <ClickCounter/>
       
       {
       /*<Counter/>
+
+      <PortalDemo/>
 
       <FRParentInput/>
 
---
+++ src/components/ClickCounter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class ClickCounter extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         Count:0
+      }
+    }
+    
+    incrementCount = ()=>{
+        this.setState(prevState =>{
+            return {Count : prevState.Count + 1}
+        })
+    }
+  render() {
+    const { Count } = this.state
+    return <button onClick={this.incrementCount}>Clicked {Count} times</button>
+  }
+}
+
+export default ClickCounter
