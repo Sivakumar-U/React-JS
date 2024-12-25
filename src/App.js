7815d0bc2d18d0e2a6ebc73a239ae4c0a0e3d9d3
Sivakumar U <itsmesivakumar21@gmail.com>
2024-12-24 19:18:13 -0500

    Created portal to display output under portal-root irrepective of root.

--- src/App.js
+++ src/App.js
@@ -23,15 +23,17 @@ import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 function App() {
   return (
     <div className="App">
-      <FRParentInput/>
-      
+      <PortalDemo/>
       
       {
       /*<Counter/>
+
+      <FRParentInput/>
 
       <FocusInput/>
 
